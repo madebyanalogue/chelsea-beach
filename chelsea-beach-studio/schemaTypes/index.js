49f90f0d370d1fca7f135213a3c528928cf0c9be
Josh Watts <josh@madebyanalogue.co.uk>
2025-08-22 13:24:53 +0100

    Restructure repository: move Nuxt app to root level for proper deployment

--- chelsea-beach-studio/schemaTypes/index.js
+++
@@ -1,8 +0,0 @@
-import news from './news'
-import page from './page'
-import section from './sections'
-import siteSettings from './siteSettings'
-import menu from './menu'
-import service from './service'
-
-export const schemaTypes = [news, page, section, siteSettings, menu, service]
